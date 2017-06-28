8d7e8909315b8b60170c92c4df7f36166ff49289
robert.minneman <robert.minneman@F1710-39RRK32-L>
2017-06-28 08:38:41 -0400

    Testing

--- RobsWorkTest/RobsWorkTest/Module1.vb
+++ RobsWorkTest/RobsWorkTest/Module1.vb
@@ -3,7 +3,7 @@
     Sub Main()
         Console.WriteLine("Michelle is the MAAAAAAAAAAAAASTAH")
         Console.WriteLine("ROB TEST TEST This is MAAAAAAAAAAAAASTAH")
-        Console.WriteLine("This is MAAAAABLAHBLAHAAAAAAAASTAH")
+        Console.WriteLine("This is TEST MAAAAABLAHBLAHAAAAAAAASTAH")
         Console.WriteLine("This is MAAAAAAAAAAAAASTAH")
     End Sub
 
